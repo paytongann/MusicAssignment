a2d97b5fd71783c8882c418a8d647430a7948a93
paytongann <paytongann@yahoo.com>
2019-08-12 19:43:35 -0400

    commit4

--- app/src/main/java/com/example/musicassignment/MainActivity.java
+++ app/src/main/java/com/example/musicassignment/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.musicassignment;
 
+import android.graphics.Color;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
@@ -11,9 +17,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity
         implements SongFragment.Communicator {
 
+    private static final String TAG = "";
     SongRepository songRepository;
     SongFragment fragment;
     ArrayList<SongFragment> queue = new ArrayList<>();
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,7 @@ public class MainActivity extends AppCompatActivity
         final ViewPager viewPager = findViewById(R.id.view_pager);
         CustomPagerAdapter pageAdapter = new CustomPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pageAdapter);
+        recyclerView = findViewById(R.id.recycler_view);
 
         SongFragment rockFragment = new SongFragment();
         rockFragment.setCommunicator(this);
@@ -42,10 +51,19 @@ public class MainActivity extends AppCompatActivity
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_black_24dp);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_dashboard_black_24dp);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_notifications_black_24dp);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+                //todo change background colors based on tab position
+                if (tab.getText().toString().equals("classic")){
+
+                } else if (tab.getText().toString().equals("pop")){
+
+                } else {
+
+                }
             }
 
             @Override
@@ -56,6 +74,8 @@ public class MainActivity extends AppCompatActivity
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+
+
         songRepository = new SongRepository(getApplication());
     }
 
